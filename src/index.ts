30e97aad83a63a66da7ab87747ea3e7d55500aa1
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan@adgorithmics.com>
2025-04-05 10:02:01 +0700

    Update Again

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use('/api', apiRoutes);
 // Base route
 app.get('/', (req, res) => {
   res.json({
-    message: 'TypeScript Node.js API is running (Test Ridwan - Update 1)',
+    message: 'TypeScript Node.js API is running (Test Ridwan - Update 1, Update Again)',
     endpoints: {
       getItems: 'GET /api/items',
       getItemById: 'GET /api/items/:id',
